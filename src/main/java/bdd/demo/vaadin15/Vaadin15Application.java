dfc07911ab9479d69fef9d87d0724345b7dc2b3f
Bhadresh Desai <bhadresh_desai@hotmail.com>
2020-05-20 21:45:04 +0100

    Initial project import from spring initializr

---
+++ src/main/java/bdd/demo/vaadin15/Vaadin15Application.java
@@ -0,0 +1,13 @@
+package bdd.demo.vaadin15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vaadin15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vaadin15Application.class, args);
+	}
+
+}
---
+++ src/test/java/bdd/demo/vaadin15/Vaadin15ApplicationTests.java
@@ -0,0 +1,13 @@
+package bdd.demo.vaadin15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vaadin15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
